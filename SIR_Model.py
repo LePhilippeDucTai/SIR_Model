9b0d02dccc3e05fc75644dae954a76a0959a7796
LePhilippeDucTai <le.philippe.duc.tai@gmail.com>
2020-03-20 17:22:13 +0100

    Added another equation for death rates.

--- SIR_Model.py
+++ SIR_Model.py
@@ -3,6 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 class SIRModel:
    def __init__(self, sir_parameters):
+      self.alpha = sir_parameters['alpha']
       self.beta = sir_parameters['beta'] 
       self.lambd = sir_parameters['lambd']
       T = sir_parameters['T']
@@ -12,30 +13,39 @@ class SIRModel:
       self.S = np.zeros(n_iter)
       self.I = np.zeros(n_iter)
       self.R = np.zeros(n_iter)
+      self.D = np.zeros(n_iter)
 
-   def initialize(self, s0, i0, r0):
+   def initialize(self, s0, i0):
       self.S[0] = s0
       self.I[0] = i0
-      self.R[0] = r0
+      self.R[0] = 0
+      self.D[0] = 0
 
-   def Survival(self, s, i):
+   def Susceptible(self, s, i):
       return -self.beta * s * i * self.dt + s
 
    def Infected(self, s, i) :
-      return self.beta * s * i * self.dt - (1 / self.lambd) * i * self.dt + i
+      return self.beta * s * i * self.dt - (1 / self.lambd + self.alpha) * i * self.dt + i
 
    def Recovered(self, i, r) :
       return (1 / self.lambd) * i * self.dt + r
 
+   def Dead(self, i, d):
+      return self.alpha * i * self.dt + d
+
    def compute(self) :
       for i in range(len(self.times) - 1):
-         self.S[i + 1] = self.Survival(self.S[i], self.I[i])
+         self.S[i + 1] = self.Susceptible(self.S[i], self.I[i])
          self.I[i + 1] = self.Infected(self.S[i], self.I[i])
          self.R[i + 1] = self.Recovered(self.I[i], self.R[i])
+         self.D[i + 1] = self.Dead(self.I[i], self.D[i])
 
    def plotcurves(self):
-      plt.plot(self.times, self.S, label = 'Survival')
-      plt.plot(self.times, self.I)
-      plt.plot(self.times, self.R)
+      plt.plot(self.times, self.S, label = "Susceptible")
+      plt.plot(self.times, self.I, label = "Infected")
+      plt.plot(self.times, self.R, label = "Recovered")
+      plt.plot(self.times, self.D, label = "Dead")
+      plt.legend()
       plt.show()
+
       
